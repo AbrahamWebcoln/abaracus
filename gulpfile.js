04462ee009bc78f54b81bff156474290e1173bd4
FrazettaSilke <abraham@abaracus.com>
2020-06-20 21:36:01 -0500

    starting out gulp file

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var cleanCss = require('gulp-clean-css')
+
+gulp.task('pack-css', function () {    
+    return gulp.src(['js/nprogress/nprogress.css', 'js/google-code-prettify/prettify.css', 'css/print.css', 'css/normalize.css', 'css/bootstrap.min.css', 'css/animate.css', 'css/fancy.min.css', 'css/flexslider.css', 'css/style.css', 'css/main.css', 'css/custom.css'])
+        .pipe(concat('main-stylesheet.css'))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', gulp.parallel('pack-css')); 
