c2f2b374ff7dcf519258eecd962cd363250055c6
Abe Coleman <abrahamcoleman@creighton.edu>
2020-07-05 16:05:09 -0500

    sharon's site

--- sharon/php/contact.php
+++ sharon/php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "sharonkammermann@gmail.com";
+$address = "abraham@abaracus.com";
 
 
 // Configuration option.
